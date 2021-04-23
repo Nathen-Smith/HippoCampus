0a119a1b5178096509bf5b551f8f6bb4818605dc
Piyush Sud <ncsmith2@illinois.edu>
2021-04-23 00:38:06 -0500

    nav bar setup, need gcp firebase

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import 'bootstrap/dist/css/bootstrap.css'
 // import Form from 'react-bootstrap/Form'
 import './App.css'
@@ -7,19 +9,41 @@ import Updates from './components/Updates.js';
 import Delete from './components/Delete.js'
 import SearchView from './components/SearchView.js'
 import AdvancedQuery from './components/AdvancedQuery.js'
+import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Create></Create><br />
-        <Updates></Updates><br />
-        <Delete></Delete><br />
-        <SearchView></SearchView><br />
-        <AdvancedQuery></AdvancedQuery><br />
+      <div className="App container py-3">
+      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+        <LinkContainer to="/">
+          <Navbar.Brand className="font-weight-bold text-muted">
+            HippoCampus
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Create></Create><br />
+      <Updates></Updates><br />
+      <Delete></Delete><br />
+      <SearchView></SearchView><br />
+      <AdvancedQuery></AdvancedQuery><br />
+      <Routes />
       </div>
+  
 
-    )
+    );
   }
 }
 
---
+++ ReactApp/src/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  );
+}
