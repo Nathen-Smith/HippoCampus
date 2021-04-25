80cf9d3f4dc2a298d70ffc713d4e54214736b26a
Kevin <>
2021-04-25 05:25:17 -0500

    trying stuff w matches tab

--- ReactApp/src/containers/Matches.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Matches() {
-  return (
-    <div className="match_tab text-center">
-      <h3>People you've matched with!</h3>
-    </div>
-  );
-}
